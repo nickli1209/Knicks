9954962cd312504d5aeeeec8342647eacfc51a1d
EC2 Default User <ec2-user@ip-172-31-59-72.us-west-2.compute.internal>
2025-12-03 22:43:46 +0000

    lets go 3

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get(`https://8ty8f17x80.execute-api.us-west-2.amazonaws.com/prd/get-person`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
